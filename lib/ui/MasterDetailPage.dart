716ecaab222b7d61e5f05ad6a7027d6a9f904041
jims57 <jims57@163.com>
2020-07-01 23:56:20 +0800

    Changed AppBar as custom one which takes more than one widget in leading position.

--- lib/ui/MasterDetailPage.dart
+++ lib/ui/MasterDetailPage.dart
@@ -11,16 +11,68 @@ class MasterDetailPage extends StatefulWidget {
 
 class _MasterDetailPageState extends State<MasterDetailPage> {
   bool isLargeScreen = false;
+  double _screenWidth = 0;
+  double _leadingWidth = 110;
+  double _tailWidth = 30;
+  double _offsetToLeft = 80;
 
   @override
   Widget build(BuildContext context) {
+//    double _offsetToLeft = (_leadingWidth-_tailWidth);
+    _screenWidth = MediaQuery.of(context).size.width;
+
+    double _titleWidth =
+        _getAppBarTitleWidth(_screenWidth, _leadingWidth, _tailWidth);
+
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('海豚笔记')),
+        title: Row(
+          children: [
+            Container(
+              width: _leadingWidth,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text('文件夹'),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              width: _titleWidth,
+              padding: const EdgeInsets.only(left: 5.0, right: 5),
+              child: Container(
+//                alignment: Alignment.center,
+//                width: _titleWidth,
+                padding: EdgeInsets.only(right: _offsetToLeft),
+                color: Colors.deepPurpleAccent,
+                child: Center(
+                  child: Text(
+//                    '英语知识英语知识英语知识英语知识英语知识英语知识英语知识英语知识英语知识英语知识英语知识英语知识知识英语知识英语知识英语知识英语知识知识英语知识英语知识英语知识英语知识知识英语知识英语知识英语知识英语知识'),
+                      '英语知识'),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.amber,
+              width: _tailWidth,
+              child: Icon(Icons.more_horiz),
+            )
+          ],
+        ),
+        titleSpacing: 0.0,
+//        centerTitle: true, //>>center title>>center appbar title
       ),
       body: OrientationBuilder(
         builder: (c, o) {
-          if (MediaQuery.of(context).size.width > 600) {
+          _screenWidth = MediaQuery.of(context).size.width;
+          if (_screenWidth > 600) {
             isLargeScreen = true;
           } else {
             isLargeScreen = false;
@@ -31,7 +83,7 @@ class _MasterDetailPageState extends State<MasterDetailPage> {
               Expanded(
                 flex: 1,
                 child: ItemListWidget(
-                    itemCount: 10,
+                    itemCount: 60,
                     onItemSelected: () {
                       // When it is a small screen
                       if (!isLargeScreen) {
@@ -54,4 +106,9 @@ class _MasterDetailPageState extends State<MasterDetailPage> {
       ),
     );
   }
+}
+
+double _getAppBarTitleWidth(
+    double screenWidth, double leadingWidth, double tailWidth) {
+  return (screenWidth - leadingWidth - tailWidth);
 }
